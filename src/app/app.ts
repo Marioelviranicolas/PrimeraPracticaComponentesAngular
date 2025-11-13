a50dc4fe6b950b7595c834b47cf3e44ee3472665
Marioelviranicolas <marioelvira73@gmail.com>
2025-11-13 14:06:44 +0100

    Subida proyecto

---
+++ src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Navbar } from './componentes/navbar/navbar';
+import { Footer } from './componentes/footer/footer';
+
+import { Body } from './componentes/body/body';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, Navbar, Footer, Body],
+  templateUrl: './app.html',
+  styleUrls: ['./app.css']
+})
+export class App {
+  title = signal('mi-proyecto');
+}
---
+++ src/app/componentes/body/body.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Card } from '../card/card'; 
+
+@Component({
+  selector: 'app-body',
+  imports: [Card],
+  templateUrl: './body.html',
+  styleUrl: './body.css',
+})
+export class Body {
+
+}
