7bd59b455fb0bf50e2b647761e1df3d220bfed19
Mario Elvira <marioelvira73@gmail.com>
2025-11-13 20:28:17 +0100

    3 componentes

--- src/app/componentes/body/body.ts
+++ src/app/componentes/body/body.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { Card } from '../card/card'; 
+
 
 @Component({
   selector: 'app-body',
-  imports: [Card],
   templateUrl: './body.html',
   styleUrl: './body.css',
 })
